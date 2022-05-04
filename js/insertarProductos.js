7e52abccc4676f6e50737b68ab1f4b01394a2431
Fernando vega <vegafernando871@gmail.com>
2022-05-03 19:11:48 -0600

    agregando los productos reales y salto de la linea en la pagina de salud y animaciones en productos

--- js/insertarProductos.js
+++ js/insertarProductos.js
@@ -3,8 +3,6 @@ const template = document.getElementById('template_productos');
 const fragment = document.createDocumentFragment();
 
 template.classList.remove('hidden');
-console.log(items.querySelector('h5').textContent);
-console.log(template.querySelector('h5').textContent);
 
 document.addEventListener('load',consultar())
 
@@ -17,23 +15,37 @@ async function consultar(){
         // console.log(data);
         imprimirproductos(data);
     }catch(error){
-        console.log(error)
+        // console.log(error)
     }
 }
 
 
 function imprimirproductos(data){
-    console.log(data);
+    // console.log(data);
+    const fr=new FileReader();
+    try {
+        console.log( fr.FileReader.readAsDataURL("../img/prod/fg.jpeg"));
+    } catch (e) {
+        console.log(e);
+    }
+    
     data.forEach(producto =>{
-        // console.log(producto);
-        template.querySelector('h5').textContent = producto.nombre;
-        template.querySelectorAll('p')[0].textContent = '$'+producto.precio;
-        template.querySelectorAll('p')[1].textContent = producto.descripcion;
-        template.querySelector('img').src = "img/prod/" + producto.foto;
+        
+        try{
+
+            // console.log(producto);
+            template.querySelector('h5').textContent = producto.nombre;
+            template.querySelectorAll('p')[0].textContent = '$'+producto.precio;
+            template.querySelectorAll('p')[1].textContent = producto.descripcion;
+            template.querySelector('img').src = "img/prod/" + producto.foto;
+            console.log( fr.FileReader.readAsDataURL("img/prod/fg.jpeg"));
+        }catch(e){
+            console.log(e);
+        }
 
         const clone = template.cloneNode('true');
         fragment.appendChild(clone);
-        console.log(clone);
+        // console.log(clone);
 
     })
     items.appendChild(fragment);
