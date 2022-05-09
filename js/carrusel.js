203878c4b956e24ee8085c3683d095d466d763e3
Fernando vega <vegafernando871@gmail.com>
2022-05-08 19:51:12 -0600

    agregando movilidad con puntos al carrusel y flechas del teclado

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,29 +1,91 @@
 const slicers = document.querySelectorAll('.carrusel_slicer');
 const flechas = document.querySelectorAll('.flechas');
-let incremento=0;
+const contenedor_puntos = document.querySelector('.contenedor_puntos_slicer')
+let current_position=0;
 
-console.log(flechas[1]);
+
+
+
+
+
+const inicializar  = function (){
+    acomodar();
+    Crear_puntos();
+    setTimeout(()=>{
+        slicers.forEach(s =>{
+            s.style.transition = 'transform ease-in-out 0.4s';
+            punto_activo();
+        })
+    }, 0)
+}
+
+const Crear_puntos = function(){
+    slicers.forEach((s,i) =>{
+        contenedor_puntos.insertAdjacentHTML('beforeend',`<div class="punto_slicer" data-slicer="${i}"></div>`);
+    })
+
+}
+
+const punto_activo = function(){
+    document.querySelectorAll('.punto_slicer').forEach((s,i)=>{
+        s.classList.remove('punto_slicer--activo')
+    })
+    document.querySelector(`.punto_slicer[data-slicer="${current_position}"]`).classList.add('punto_slicer--activo')
+}
+
+
+
+
+
+const acomodar = function (){
+    // console.log(current_position, "acomodar")
+    slicers.forEach(function(s,i) {
+        s.style.transform = `translateX(${(i-current_position)*100}%)`
+    })   
+    document.querySelector(`.punto_slicer[data-slicer="${current_position}"]`) &&  punto_activo();
+    
+}
+
+const derecha = function(){
+    current_position++;
+    if(current_position>slicers.length-1)current_position=0;
+    acomodar();
+    
+}
+
+const izquierda = function(){
+    current_position--;
+    if(current_position<0)current_position=slicers.length-1;
+    acomodar();
+   
+}
+
+
+
+inicializar()
+
+// console.log(flechas[1]);
 flechas[0].addEventListener('click',izquierda)
 flechas[1].addEventListener('click',derecha)
 
+// creando el carrusel con las flechas
+document.addEventListener('keydown', e =>{
+    if(e.key === 'ArrowLeft')izquierda();
+    if(e.key === 'ArrowRight')derecha();
 
-function derecha(){
-    incremento--;
-    if(incremento<-3)incremento=0;
+})
+
+//respondiendo a los botones 
+contenedor_puntos.addEventListener('click', e =>{
+    // console.log(e.target.dataset.slicer)
+    
+    if(e.target.dataset.slicer){
+        current_position = e.target.dataset.slicer;
     acomodar();
-}
-
-function izquierda(){
-    incremento++;
-    if(incremento>0)incremento=-3;
-    acomodar();
-}
+    
+    }
+})
 
 
-function acomodar(){
-    slicers.forEach(function(s,i) {
-        s.style.transform = `translateX(${(i+incremento)*100}%)`
-        console.log(s,i)
-    })
-}
-acomodar()
+
+
