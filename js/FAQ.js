da0d956c838b13d0da6ddf24acb74ac2ad94a7d9
Fernando vega <vegafernando871@gmail.com>
2022-05-20 16:26:09 -0600

    modificando pagina FAQ estilos curso UDEMY, agregando un footer nuevo

--- js/FAQ.js
+++ js/FAQ.js
@@ -1,9 +1,12 @@
 
 const categorias = document.querySelector('.categorias');
-const preguntas = document.querySelector('.preguntas');
+const preguntas = document.querySelector('[data-preguntas="preguntas"]');
+const body = document.querySelector('body')
+
+
 let categoriaElegida;
 // console.log(categorias)
-// console.log(preguntas)
+console.log(preguntas)
 
 
 
@@ -11,7 +14,12 @@ async  function imprimirCategorias(){
     const peticion = await fetch('js/categorias.json');
     const data = await peticion.json();
     data.forEach(categoria => {
-        categorias.insertAdjacentHTML('beforeend',`<h1>${categoria.cveCategoria}</h1> `)
+        categorias.insertAdjacentHTML('beforeend',`
+        
+        <div class="categoria">
+        <h1 class="nombre_categoria">${categoria.cveCategoria}</h1> 
+        </div>
+        `)
     });
     categoriaElegida = data[0]?.cveCategoria &&'TERAP';
     
@@ -27,13 +35,22 @@ async  function imprimirPreguntas(){
         preguntas.removeChild(preguntas.firstChild);
       };
     
-    data.forEach(categoria => {
+    data.forEach((categoria,i) => {
         if(categoriaElegida == categoria.cveCategoria)
         preguntas.insertAdjacentHTML('beforeend',`
-        <h1 class="pregunta">${categoria.pregunta}</h1> 
-        <P class="respuesta">${categoria.respuesta}</P>
+        <div class="contenedor_pregunta" data-pregunta="${i}">
+            <div class="contenedor_icon">
+                <h1 class="pregunta">${categoria.pregunta}</h1>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="icon" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                </svg> 
+            </div>
+            <P class="respuesta">${categoria.respuesta}</P>
+        </div>
         `);
     });
+    console.log(preguntas)
+    
     
 
 }
@@ -49,3 +66,25 @@ categorias.addEventListener('click', e =>{
     imprimirPreguntas();
 
 })
+
+body.addEventListener('mouseover', e =>{
+    const preguntas = document.querySelectorAll('[data-pregunta]')
+    console.log(preguntas)
+    if(!e.target.closest('[data-pregunta]')){
+        preguntas.forEach((pregunta)=>{
+            console.log(pregunta)
+        pregunta.querySelector('.respuesta').style = "max-height: 0;opacity:0;"
+        pregunta.querySelector('.icon').style = "transform:rotate(0deg);"
+        })
+        
+        return;
+    }
+
+    const padre = e.target.closest('[data-pregunta]');
+    console.log(e.target.closest('[data-pregunta]'));
+    padre.querySelector('.respuesta').style = "max-height: 500px;opacity:1;"
+    padre.querySelector('.icon').style = "transform:rotate(180deg);stroke:var(--verde_primario);"
+    console.log(padre.querySelector('.respuesta'));
+    
+})
+
